bd18389aa94816d266942e11e1e581bbc4e608cb
SUHAIB SABIR <suhaibsabir110@gmail.com>
2024-04-24 13:47:17 -0700

    aa
    a

--- index.ts
+++ index.ts
@@ -8,3 +8,17 @@ var age: number = 123;
 var rollNumber: number = 132125;
 
 console.log(names, age, rollNumber)
+
+
+
+function check(str) {
+
+    for (var i = 0; i <= str.length; i++) {
+        if (str[i] % 2 === 0) {
+            console.log("this is a even")
+        }
+    }
+
+    return str;
+}
+console.log(check(12345))
