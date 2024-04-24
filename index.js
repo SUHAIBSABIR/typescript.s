77bd92b3d5de4dd54b41674f3790531b031b9dbb
SUHAIB SABIR <suhaibsabir110@gmail.com>
2024-04-24 13:38:36 -0700

    s
    s

---
+++ index.js
@@ -0,0 +1,8 @@
+// function sums(a: number, b: number) {
+//     return a + b;
+// }
+// console.log(sums(10, 10))
+var names = "suhaib";
+var age = 123;
+var rollNumber = 132125;
+console.log(names, age, rollNumber);
---
+++ index.ts
@@ -0,0 +1,10 @@
+// function sums(a: number, b: number) {
+//     return a + b;
+// }
+// console.log(sums(10, 10))
+
+var names: string = "suhaib"
+var age: number = 123;
+var rollNumber: number = 132125;
+
+console.log(names, age, rollNumber)
